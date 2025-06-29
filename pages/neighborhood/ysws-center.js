a213fc2b4351d21a097486b00b2e88a32d5f87c8
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2025-06-29 14:38:54 -0700

    Update ysws-center.js

--- pages/neighborhood/ysws-center.js
+++ pages/neighborhood/ysws-center.js
@@ -50,6 +50,7 @@ export default function YSWSCenter() {
         firstName: sub.firstName,
         lastName: sub.lastName,
         slackId: Array.isArray(sub.slackId) ? sub.slackId[0] : sub.slackId,
+        slackRealId: Array.isArray(sub.slackRealId) ? sub.slackRealId[0] : sub.slackRealId,
         pfpUrl: Array.isArray(sub.Pfp) && sub.Pfp.length > 0 ? sub.Pfp[0].url : null,
         githubUsername: sub.githubUsername
       }))

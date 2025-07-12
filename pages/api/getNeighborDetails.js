7d0eb7acd19634828a481f35cf81dcb67ea5a540
Max Wofford <max@maxwofford.com>
2025-07-12 13:26:57 -0700

    Add handle during initial data loading for getNeighborDetails

--- pages/api/getNeighborDetails.js
+++ pages/api/getNeighborDetails.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
           "Pfp (from slackNeighbor)",
           "Slack ID (from slackNeighbor)",
           "Full Name (from slackNeighbor)",
+          "Slack Handle (from slackNeighbor)",
           "githubUsername",
           "totalTimeCombinedHours",
           "totalTimeHackatimeHours",
